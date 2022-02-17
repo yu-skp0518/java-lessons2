9136f95e027d2cafb58e6301683d8209b603aa57
yu-skp0518 <reality_0518@yahoo.co.jp>
2022-02-17 14:51:13 +0000

    review_2

--- MyApp.java
+++ MyApp.java
@@ -1,7 +1,28 @@
 public class MyApp {
 
   public static void main(String[] args) {
-    System.out.println("hello world");
+
+    int i;
+
+    i = 10 / 3;
+    System.out.println(i);
+
+    i = 10 % 3;
+    System.out.println(i);
+
+
+    int x = 5;
+    x ++;
+    System.out.println(x);
+
+    x --;
+    System.out.println(x);
+
+    x += 12;
+    System.out.println(x);
+
+    String s = "hello " + "world";
+    System.out.println(s);
   }
 
 }
