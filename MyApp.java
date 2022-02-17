268e95caeab4dca089751946923f5e79cd3b5a28
yu-skp0518 <reality_0518@yahoo.co.jp>
2022-02-17 15:27:56 +0000

    review_4

--- MyApp.java
+++ MyApp.java
@@ -1,21 +1,9 @@
 public class MyApp {
-
   public static void main(String[] args) {
 
-    int score = 90;
-    // if (score > 80) {
-    //   System.out.println("Great!");
-    // } else if (score > 60) {
-    //   System.out.println("Good!");
-    // } else {
-    //   System.out.println("so so ...");
-    // }
-
-
-
-    String msg = score > 80 ? "Great!" : "so so...";
-    System.out.println(msg);
+    for (int i = 0; i < 10; i++) {
+      System.out.println(i);
+    }
 
   }
-
 }
