d1ffe8aa669cdeb26fd22ff7be1c4d2e1230e6e6
yu-skp0518 <reality_0518@yahoo.co.jp>
2022-02-17 15:38:13 +0000

    review_5

--- MyApp.java
+++ MyApp.java
@@ -1,9 +1,12 @@
 public class MyApp {
   public static void main(String[] args) {
 
-    for (int i = 0; i < 10; i++) {
-      System.out.println(i);
-    }
+  int[] sales = new int[] {100, 200, 300};
+
+    System.out.println(sales[2]);
+
+    sales[2] = 1000;
+    System.out.println(sales[2]);
 
   }
 }
